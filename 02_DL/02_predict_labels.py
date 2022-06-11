corrected transfer learning to first have the last layer trained with earlier weights being frozen and additional fine tuning. PatchCROPDataModule: Added correct normalization for transfer learning. Added TensorDataset with augmentation support. Random flip augmentation now onyl affects the training set. Updated some hyperparameters.

02_DL/02_predict_labels.py
@@ -15,16 +15,16 @@ from sklearn.metrics import r2_score
 import torch
 from torch.utils.data.dataloader import DataLoader
 
-from pytorch_lightning import seed_everything, Trainer
-from pytorch_lightning.callbacks import Callback, EarlyStopping
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning import seed_everything#, Trainer
+# from pytorch_lightning.callbacks import Callback, EarlyStopping
+# from pytorch_lightning.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader, Dataset, TensorDataset, random_split
 
 
 # Own modules
 from PatchCROPDataModule import PatchCROPDataModule#, KFoldLoop#, SpatialCVModel
 from RGBYieldRegressor import RGBYieldRegressor
-from MCYieldRegressor import MCYieldRegressor
+from MC_YieldRegressor import MCYieldRegressor
 
 
 
@@ -38,11 +38,11 @@ __email__ = 'stefan.stiller@zalf.de, stillsen@gmail.com'
 __status__ = 'Dev'
 
 
-class PrintCallback(Callback):
-    def on_train_start(self, trainer, pl_module):
-        print("Training is started!")
-    def on_train_end(self, trainer, pl_module):
-        print("Training is done.")
+# class PrintCallback(Callback):
+#     def on_train_start(self, trainer, pl_module):
+#         print("Training is started!")
+#     def on_train_end(self, trainer, pl_module):
+#         print("Training is done.")
 
 #############################################################################################
 #                           Step 5 / 5: Connect the KFoldLoop to the Trainer                #
@@ -60,10 +60,10 @@ if __name__ == "__main__":
     input_files = dict()
     input_files_rgb = dict()
 
-    data_root = '/beegfs/stiller/PatchCROP_all/Data/'
-    # data_root = '../../2_Data_preprocessed/2977x_Raster_Rescaled_Labels_and_Features__Analyses_Packages_for_HPC/'
-    output_root = '/beegfs/stiller/PatchCROP_all/Output/'
-    # output_root = '/media/stillsen/Hinkebein/PatchCROP/AIA/2022_Explaining_Heterogeneity_in_Crop_Yield_Prediction_using_Remote_and_Proximal_Sensing/Output/'
+    # data_root = '/beegfs/stiller/PatchCROP_all/Data/'
+    data_root = '../../2_Data_preprocessed/2977x_Raster_Rescaled_Labels_and_Features__Analyses_Packages_for_HPC/'
+    # output_root = '/beegfs/stiller/PatchCROP_all/Output/'
+    output_root = '../../Output/'
 
     ## Patch 12
     output_dirs[12] = os.path.join(output_root, 'Patch_ID_12')
@@ -78,7 +78,9 @@ if __name__ == "__main__":
         'Tempelberg_Soda_22062020_transparent_mosaic_group1_merged_aligned_Patch_ID_12.tif']}
 
     ## Patch 73
-    output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB_densenet_not_augmented_custom_btf')
+    # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB_densenet_not_augmented_custom_btf_art_labels_green_wd0_b200')
+    output_dirs[73] = os.path.join(output_root,
+                                   'Patch_ID_73_RGB_densenet_not_augmented_custom_btf_art_labels_green')
     # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB+_densenet_augmented_custom')
     # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_densenet')
 
@@ -146,19 +148,20 @@ if __name__ == "__main__":
     num_folds = 9
 
     patch_no = 73
+    stride = 20
     architecture = 'densenet'
     # architecture = 'resnet50'
-    augmentation = False
+    augmentation = True
     tune_fc_only = False
     features = 'RGB'
     # features = 'RGB+'
-    num_samples = None
+    num_samples_per_fold = None
 
     this_output_dir = output_dirs[patch_no]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-    datamodule = PatchCROPDataModule(input_files=input_files_rgb[patch_no], data_dir=data_dirs[patch_no], stride=10, workers=os.cpu_count(), augmented=augmentation, batch_size=1)
+    datamodule = PatchCROPDataModule(input_files=input_files_rgb[patch_no], patch_id=patch_no, data_dir=data_dirs[patch_no], stride=stride, workers=os.cpu_count(), augmented=augmentation, input_features=features, batch_size=1)
 
     checkpoint_paths = ['model_f0.ckpt',
                         'model_f1.ckpt',
@@ -172,7 +175,10 @@ if __name__ == "__main__":
                         ]
     checkpoint_paths = [output_dirs[patch_no]+'/'+cp for cp in checkpoint_paths]
 
-    datamodule.prepare_data()
+    if num_samples_per_fold == None:
+        datamodule.prepare_data(num_samples=num_samples_per_fold)
+    else:
+        datamodule.load_subsamples(num_samples=num_samples_per_fold)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -191,17 +197,21 @@ if __name__ == "__main__":
         global_pred[s] = []
         global_y[s] = []
         local_r2[s] = []
+        # i=0
         for k in range(num_folds):
+            # ####### DEBUG
+            # if k>0:
+            #     break
             # load model and set to eval mode
-            model_wrapper.model.load_state_dict(torch.load(checkpoint_paths[k]))
+            model_wrapper.model.load_state_dict(torch.load(checkpoint_paths[k],map_location=torch.device('cpu')))
             model_wrapper.model.to(device)
             model_wrapper.model.eval()
             # load data
             datamodule.setup_fold_index(k)
             if s == 'train':
-                dl = datamodule.train_dataloader(num_samples=num_samples)
+                dl = datamodule.train_dataloader()
             elif s == 'val':
-                dl = datamodule.val_dataloader(num_samples=num_samples)
+                dl = datamodule.val_dataloader()
 
 
             # for each fold store labels and predictions
@@ -215,14 +225,27 @@ if __name__ == "__main__":
                 # make prediction
                 with torch.no_grad():
                     y_hat = torch.flatten(model_wrapper.model(inputs))
-                # if s == 'train':
-                #     print('{} - {}'.format(labels.detach().cpu().numpy(), y_hat.detach().cpu().numpy()))
+                # if s == 'train' and i<=20:
+                #     print('{}. {} - {}'.format(i, labels.detach().cpu().numpy(), y_hat.detach().cpu().numpy()))
+                #     i+=1
+                # else:
+                #     print('other')
+
+                    # if i >21:
+                    #     break
                 # while looping over the data loader save labels and predictions
                 local_preds.extend(y_hat.detach().cpu().numpy())
                 local_labels.extend(labels.detach().cpu().numpy())
+            print(local_preds)
+            pass
             # save labels and predictions for each fold
             torch.save(local_preds, os.path.join(this_output_dir, 'y_hat_' + s + '_' + str(k) + '.pt'))
             torch.save(local_labels, os.path.join(this_output_dir, 'y_' + s + '_' + str(k) + '.pt'))
+
+            # for debugging, save labels and predictions in df
+            y_yhat_df = pd.DataFrame({'y':local_labels, 'y_hat':local_preds})
+            y_yhat_df.to_csv(os.path.join(this_output_dir, 'y-y_hat_{}.csv'.format(k)), encoding='utf-8')
+
             # save local r2 for a certain train or val set
             local_r2[s].append(r2_score(local_labels, local_preds))
             print(''+s+':: {}-fold local R2 {}'.format(k, local_r2[s][k]))
@@ -231,9 +254,9 @@ if __name__ == "__main__":
             global_y[s].extend(local_labels)
         global_r2[s] = r2_score(global_y[s], global_pred[s])
         print(''+s+':: global R2 {}'.format(global_r2))
-        # save predictions to CSV
-        # statistics_df = pd.read_csv(os.path.join(output_root, 'Performance_all_Patches_RGB.csv'))
-        statistics_df.loc[-1] = [patch_no, features, architecture, s, global_r2[s], local_r2[s][0], local_r2[s][1], local_r2[s][2], local_r2[s][3], local_r2[s][4], local_r2[s][5], local_r2[s][6], local_r2[s][7], local_r2[s][8]]
-        statistics_df.index = statistics_df.index + 1
-        statistics_df.sort_index()
-    statistics_df.to_csv(os.path.join(this_output_dir, 'prediction_statistics.csv'), encoding='utf-8')
+    #     # save predictions to CSV
+    #     # statistics_df = pd.read_csv(os.path.join(output_root, 'Performance_all_Patches_RGB.csv'))
+    #     statistics_df.loc[-1] = [patch_no, features, architecture, s, global_r2[s], local_r2[s][0], local_r2[s][1], local_r2[s][2], local_r2[s][3], local_r2[s][4], local_r2[s][5], local_r2[s][6], local_r2[s][7], local_r2[s][8]]
+    #     statistics_df.index = statistics_df.index + 1
+    #     statistics_df.sort_index()
+    # statistics_df.to_csv(os.path.join(this_output_dir, 'prediction_statistics.csv'), encoding='utf-8')

02_DL/RGBYieldRegressor.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Crop yield regression model implementing a ResNet50 in PyTorch Lightning in a transfer learning approach using
+Wrapper class for crop yield regression model employing a ResNet50 or densenet in PyTorch in a transfer learning approach using
 RGB remote sensing observations
 """
 
@@ -10,9 +10,8 @@ RGB remote sensing observations
 import torch
 import torchvision.models as models
 import torch.nn as nn
-from torch.optim import SGD, Adam
 
-from pytorch_lightning.core.lightning import LightningModule
+# from pytorch_lightning.core.lightning import LightningModule
 
 # Own modules
 
@@ -28,19 +27,14 @@ __status__ = 'Dev'
 
 # class RGBYieldRegressor(LightningModule):
 class RGBYieldRegressor:
-    def __init__(self, optimizer:str = 'sgd', k:int = 0, lr:float = 0.001, momentum:float = 0.8, wd:float = 0.01, batch_size:int = 16, pretrained:bool = True, tune_fc_only:bool = False, model: str = 'resnet50'):
-        super().__init__()
+    def __init__(self, k:int = 9, lr:float = 0.001, momentum:float = 0.8, wd:float = 0.01, batch_size:int = 16, pretrained:bool = True, tune_fc_only:bool = True, model: str = 'densenet'):
+        # super(RGBYieldRegressor, self).__init__()
 
         self.lr = lr
         self.momentum = momentum
         self.wd = wd
         self.batch_size = batch_size
         self.k = k
-
-        optimizers = {'adam': Adam, 'sgd': SGD}
-        self.optimizer = optimizers[optimizer]
-
-        self.criterion = nn.MSELoss(reduction='mean')
 
         self.model_arch = model
 
@@ -67,6 +61,27 @@ class RGBYieldRegressor:
                 for child in list(self.model.children())[:-1]:
                     for param in child.parameters():
                         param.requires_grad = False
+
+        self.optimizer = torch.optim.SGD(params=self.model.parameters(),
+                                            lr=lr,
+                                            momentum=momentum,
+                                            weight_decay=wd)
+
+        self.criterion = nn.MSELoss(reduction='mean')
+
+
+    def enable_grads(self):
+        for child in list(self.model.children()):
+            for param in child.parameters():
+                param.requires_grad = True
+
+    def update_optimizer(self, lr, momentum, wd):
+        self.optimizer = torch.optim.SGD(params=self.model.parameters(),
+                                         lr=lr,
+                                         momentum=momentum,
+                                         weight_decay=wd)
+    def update_criterion(self, loss: torch.nn.modules.loss):
+        self.criterion = loss
 
     # def forward(self, x):
     #     return torch.flatten(self.model(x))

02_DL/plot_sets_PatchCROPDataModule.py
@@ -7,14 +7,14 @@ script to train model
 import time
 import os
 from copy import deepcopy
-import random
 
 # Libs
-# from pytorch_lightning import seed_everything, Trainer
+from pytorch_lightning import seed_everything, Trainer
 from sklearn.metrics import r2_score
 import numpy as np
 import  pandas as pd
-
+import seaborn as sns
+from matplotlib import pyplot as plt
 import torch
 from torch import nn
 import torchvision.models as models
@@ -22,6 +22,7 @@ import torchvision.models as models
 from pytorch_lightning.callbacks import Callback, EarlyStopping, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader, Dataset, TensorDataset, random_split
+import torchvision.transforms as transforms
 
 # Own modules
 from PatchCROPDataModule import PatchCROPDataModule
@@ -40,7 +41,7 @@ __email__ = 'stefan.stiller@zalf.de, stillsen@gmail.com'
 __status__ = 'Dev'
 
 
-def train_model(model, dataloaders, criterion, optimizer, device, patience:int =20, delta:float =0, num_epochs=25):
+def train_model(model, dataloaders, criterion, optimizer, device, patience:int =10, delta:float =0, num_epochs=25):
     since = time.time()
     val_mse_history = []
     train_mse_history = []
@@ -66,13 +67,8 @@ def train_model(model, dataloaders, criterion, optimizer, device, patience:int =
                 model.eval()   # Set model to evaluate mode
             running_loss = 0.0
 
-            # print('phase: {}'.format(phase))
             # Iterate over data.
-            counter = 0
             for inputs, labels in dataloaders[phase]:
-                # print(counter)
-                # counter += 1
-                #
                 inputs = inputs.to(device)
                 labels = labels.to(device)
                 # zero the parameter gradients
@@ -82,7 +78,6 @@ def train_model(model, dataloaders, criterion, optimizer, device, patience:int =
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs)
                     loss = criterion(torch.flatten(outputs), labels.data)
-                    # loss = criterion(outputs, labels.data)
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
@@ -92,21 +87,22 @@ def train_model(model, dataloaders, criterion, optimizer, device, patience:int =
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             print('{} Loss: {:.4f}'.format(phase, epoch_loss))
 
-            if phase == 'train':
-            # if phase == 'val':
-                if (epoch_loss - best_loss) < delta:
-                    print('saving best model')
-                    best_loss = epoch_loss
-                    best_model_wts = deepcopy(model.state_dict())
-                    best_epoch = epoch
-                    patience = 20
-                else:#no improvement
-                    print('patience: {}'.format(patience))
-                    patience -= 1
+            if phase == 'train' and epoch_loss < best_loss:
+            # if phase == 'val' and epoch_loss < best_loss:
+                print('saving best model')
+                best_loss = epoch_loss
+                best_model_wts = deepcopy(model.state_dict())
+                best_epoch = epoch
+                patience = 10
+            if phase == 'val' and (epoch_loss-best_loss) > delta:#epoch_loss > best_loss:
+                print('patience: {}'.format(patience))
+                patience -= 1
             if phase == 'val':
                 val_mse_history.append(epoch_loss)
             if phase == 'train':
                 train_mse_history.append(epoch_loss)
+
+        print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
@@ -133,21 +129,17 @@ def compare_models(model_1, model_2):
 
 
 if __name__ == "__main__":
-    # seed_everything(42)
-    torch.manual_seed(42)
-    random.seed(42)
-    np.random.seed(42)
-
+    seed_everything(42)
 
     output_dirs = dict()
     data_dirs = dict()
     input_files = dict()
     input_files_rgb = dict()
 
-    data_root = '/beegfs/stiller/PatchCROP_all/Data/'
-    # data_root = '../../2_Data_preprocessed/2977x_Raster_Rescaled_Labels_and_Features__Analyses_Packages_for_HPC/'
-    output_root = '/beegfs/stiller/PatchCROP_all/Output/'
-    # output_root = '/media/stillsen/Hinkebein/PatchCROP/AIA/2022_Explaining_Heterogeneity_in_Crop_Yield_Prediction_using_Remote_and_Proximal_Sensing/Output/'
+    # data_root = '/beegfs/stiller/PatchCROP_all/Data/'
+    data_root = '../../2_Data_preprocessed/2977x_Raster_Rescaled_Labels_and_Features__Analyses_Packages_for_HPC/'
+    # output_root = '/beegfs/stiller/PatchCROP_all/Output/'
+    output_root = '../../Output/'
 
     ## Patch 12
     output_dirs[12] = os.path.join(output_root,'Patch_ID_12')
@@ -161,11 +153,11 @@ if __name__ == "__main__":
 
 
     ## Patch 73
-    # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB_densenet_not_augmented_custom_btf_test')
+    # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB+_densenet_augmented_custom')
     output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB_densened_augmented_custom_btf_art_labels_hyper')
     # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB_densenet_not_augmented_custom_btf_s3000')
     # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB_densenet_augmented_custom_s2000')
-    # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_RGB+_densenet_augmented_custom_btf_s1000')
+    # output_dirs[73] = os.path.join(output_root, 'Patch_ID_73_densenet')
 
     # data_dirs[73] = os.path.join(data_root, 'Patch_ID_73_0307')
     data_dirs[73] = os.path.join(data_root, 'Patch_ID_73')
@@ -180,17 +172,6 @@ if __name__ == "__main__":
     # input_files_rgb[73] = {'pC_col_2020_plant_PS473_SOats_smc_Krig.tif': [#'Tempelberg_soda3D_03072020_transparent_mosaic_group1_Patch_ID_73.tif',
     #                                                                       'Tempelberg_Soda_22062020_transparent_mosaic_group1_merged_aligned_Patch_ID_73.tif',
     #                                                                       'Tempelberg_sequ_11062020_index_ndvi_aligned_Patch_ID_73.tif',
-    #                                                                       ]}
-
-    ## Patch 119
-    output_dirs[119] = os.path.join(output_root, 'Patch_ID_119_RGB')
-    # data_dirs[119] = os.path.join(data_root, 'Patch_ID_73_0307')
-    # data_dirs[119] = os.path.join(data_root, 'Patch_ID_119')
-    data_dirs[119] = os.path.join(data_root, 'Patch_ID_119')
-
-    input_files_rgb[119] = {'pC_col_2020_plant_PS4119_Lup_smc_Krig.tif': ['Tempelberg_Soda_22062020_transparent_mosaic_group1_merged_aligned_Patch_ID_119.tif']}
-    # input_files_rgb[119] = {'pC_col_2020_plant_PS4119_Lup_smc_Krig.tif': ['Tempelberg_Soda_22062020_transparent_mosaic_group1_merged_aligned_Patch_ID_119.tif',
-    #                                                                       'Tempelberg_sequ_11062020_index_ndvi_aligned_Patch_ID_119.tif',
     #                                                                       ]}
 
     ## Patch 39
@@ -242,18 +223,16 @@ if __name__ == "__main__":
                                                      'Tempelberg_Soda_22062020_transparent_mosaic_group1_merged_aligned_Patch_ID_89.tif']}
 
     ## HYPERPARAMETERS
-    num_epochs = 200
-    num_epochs_finetuning = 10
+    num_epochs = 10
     lr = 0.001
-    lr_finetuning = 0.0001
-    momentum = 0.9
-    wd = 0#0.01
+    momentum = 0.8
+    wd = 0.01
     classes = 1
-    batch_size = 200#20
-    num_folds = 9
+    batch_size = 1
+    num_folds = 1
 
     patch_no = 73
-    stride = 10 # 20 is too small
+    stride= 20
     architecture = 'densenet'
     # architecture = 'resnet50'
     augmentation = True
@@ -264,131 +243,113 @@ if __name__ == "__main__":
 
     this_output_dir = output_dirs[patch_no]
 
-    print('Setting up data in {}'.format(data_dirs[patch_no]))
-    datamodule = PatchCROPDataModule(input_files=input_files_rgb[patch_no], patch_id=patch_no, data_dir=data_dirs[patch_no], stride=stride, workers=os.cpu_count(), augmented=augmentation, input_features=features, batch_size=batch_size)
+    datamodule = PatchCROPDataModule(input_files=input_files_rgb[patch_no], patch_id=patch_no,
+                                     data_dir=data_dirs[patch_no], stride=stride, workers=os.cpu_count(),
+                                     augmented=augmentation, input_features=features, batch_size=batch_size)
     datamodule.prepare_data(num_samples=num_samples_per_fold)
+    # datamodule.load_subsamples(num_samples=num_samples_per_fold)
 
-    print('working on patch {}'.format(patch_no))
+    if features == 'RGB':
+        mean_norm = torch.tensor([0.485, 0.456, 0.406])
+        std_norm = torch.tensor([0.229, 0.224, 0.225])
+    elif features == 'RGB+':
+        mean = torch.tensor([0.485, 0.456, 0.406, 0])
+        std = torch.tensor([0.229, 0.224, 0.225, 1])
     # loop over folds, last fold is for testing only
-
-
-    ############################### DEBUG
-    # for k in range(num_folds):
-    for k in range(1):
+    k_sets = []
+    train_sets = []
+    for k in range(num_folds):
+        print('-'*20)
         print(f"STARTING FOLD {k}")
-        # Detect if we have a GPU available
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print('working on device %s' % device)
+        # # Detect if we have a GPU available
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # print('working on device %s' % device)
+        #
+        # if features == 'RGB':
+        #     model_wrapper = RGBYieldRegressor(pretrained=True, tune_fc_only=tune_fc_only, model=architecture)
+        # elif features == 'RGB+':
+        #     model_wrapper = MCYieldRegressor(pretrained=True, tune_fc_only=tune_fc_only, model=architecture)
 
-        # data
         datamodule.setup_fold_index(k)
-        dataloaders_dict = {'train': datamodule.train_dataloader(), 'val': datamodule.val_dataloader()}
-
-        #### TUNE LAST LAYER, FREEZE BEFORE::
-        if features == 'RGB':
-            model_wrapper = RGBYieldRegressor(pretrained=True, tune_fc_only=True, model=architecture, lr=lr, wd=wd, momentum=momentum)
-        elif features == 'RGB+':
-            model_wrapper = MCYieldRegressor(pretrained=True, tune_fc_only=True, model=architecture, lr=lr, wd=wd)
-
-        # Send the model to GPU
-        model_wrapper.model.to(device)
-
-        # Train and evaluate
-        print('training for {} epochs'.format(num_epochs))
-        best_model, val_losses, train_losses, best_epoch = train_model(model=model_wrapper.model,
-                                                                       dataloaders=dataloaders_dict,
-                                                                       criterion=model_wrapper.criterion,
-                                                                       optimizer=model_wrapper.optimizer,
-                                                                       delta=0,
-                                                                       patience=10,
-                                                                       device=device,
-                                                                       num_epochs=num_epochs,
-                                                                       )
-        #### Fine-tuning all layers::
-        model_wrapper.model = best_model
-        model_wrapper.enable_grads()
-        model_wrapper.update_criterion(nn.MSELoss(reduction='mean'))
-        model_wrapper.update_optimizer( lr=lr_finetuning, momentum=momentum, weight_decay=wd)
-        # Train and evaluate
-        print('fine-tuning all layers for {} epochs'.format(num_epochs_finetuning))
-        best_model, ft_val_losses, ft_train_losses, ft_best_epoch = train_model(model=model_wrapper.model,
-                                                                       dataloaders=dataloaders_dict,
-                                                                       criterion=model_wrapper.criterion,
-                                                                       optimizer=model_wrapper.optimizer,
-                                                                       delta=0,
-                                                                       patience=5,
-                                                                       device=device,
-                                                                       num_epochs=num_epochs_finetuning,
-                                                                       )
+        dl = datamodule.val_dataloader()
+        k_set = []
+        i=0
+        for inputs, labels in dl:
+            k_set.extend(labels.numpy())
+            if i<8:
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                ax.imshow(inputs.squeeze().permute(1,2,0)*std_norm+mean_norm)
+                plt.savefig(os.path.join(this_output_dir, 'input_val_f{}_{}.png'.format(k,i)))
+                i+=1
+        # k_sets.append(datamodule.splits[k].datasets[-1].tensors[1].numpy())
+        k_sets.append(k_set)
+        print('Test set mean: {}'.format(np.mean(k_set)))
+        print('Test set SD: {}'.format(np.std(k_set)))
+        print('Test set Max: {}'.format(max(k_set)))
 
 
-        # save best model
-        torch.save(best_model.state_dict(), os.path.join(this_output_dir, 'model_f' + str(k) + '.ckpt'))
-        # save training statistics
-        df = pd.DataFrame({'train_loss': train_losses,
-                           'val_loss': val_losses,
-                           'best_epoch': best_epoch,
-                           'ft_val_loss':ft_val_losses,
-                           'ft_train_loss':ft_train_losses,
-                           'ft_best_epoch':ft_best_epoch})
-        df.to_csv(os.path.join(this_output_dir, 'training_statistics_f' + str(k) + '.csv'), encoding='utf-8')
 
-        ## compare models
+        # dataloaders_dict = {'train': datamodule.train_dataloader(), 'val': datamodule.val_dataloader()}
+
+        dl = datamodule.train_dataloader()
+        train_labels = []
+        i=0
+        for inputs, labels in dl:
+            train_labels.extend(labels.numpy())
+            if i<8:
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                ax.imshow(inputs.squeeze().permute(1,2,0)*std_norm+mean_norm)
+                plt.savefig(os.path.join(this_output_dir, 'input_train_f{}_{}.png'.format(k,i)))
+                i+=1
+
+        train_sets.append(train_labels)
+        print('Train set mean: {}'.format(np.mean(train_labels)))
+        print('Train set SD: {}'.format(np.std(train_labels)))
+        print('Train set Max: {}'.format(max(train_labels)))
+
+    all_labels_val = []
+    all_k = []
+    for k in range(num_folds):
+        all_labels_val.extend(k_sets[k])
+        all_k.extend(np.ones(len(k_sets[0]))*k)
+    df_dict = {'l': all_labels_val, 'k': all_k}
+    df = pd.DataFrame(df_dict)
+
+    fig = plt.figure()
+    ax = sns.violinplot(x="k", y="l", data=df)
+    fig.savefig(os.path.join(this_output_dir, 'val_folds_dist_{}.png'.format(num_samples_per_fold)))
+
+    all_labels_train = []
+    all_k = []
+    for k in range(num_folds):
+        all_labels_train.extend(train_sets[k])
+        all_k.extend(np.ones(len(train_sets[k])) * k)
+    df_dict = {'l': all_labels_train, 'k': all_k}
+    df = pd.DataFrame(df_dict)
+
+    fig = plt.figure()
+    ax = sns.violinplot(x="k", y="l", data=df)
+    fig.savefig(os.path.join(this_output_dir, 'train_folds_dist_{}.png'.format(num_samples_per_fold)))
+
+        # # Send the model to GPU
+        # model_wrapper.model.to(device)
+        #
+        # loss_fn = nn.MSELoss(reduction='mean')
+        # optimizer = torch.optim.SGD(model_wrapper.model.parameters(), lr=lr, momentum=momentum, weight_decay=wd)
+        # # Train and evaluate
+        # print('training for {} epochs'.format(num_epochs))
+        # best_model, val_losses, train_losses, best_epoch = train_model(model_wrapper.model, dataloaders_dict, loss_fn, optimizer, delta=0.1, device=device, num_epochs=num_epochs, )
+        # # save best model
+        # torch.save(best_model.state_dict(), os.path.join(this_output_dir, 'model_f' + str(k) + '.ckpt'))
+        # # save training statistics
+        # df = pd.DataFrame({'train_loss': train_losses, 'val_loss': val_losses, 'best_epoch': best_epoch})
+        # df.to_csv(os.path.join(this_output_dir, 'training_statistics_f' + str(k) + '.csv'), encoding='utf-8')
+        #
         # load_model = RGBYieldRegressor(pretrained=True, tune_fc_only=tune_fc_only, model=architecture)
         # load_model.model.load_state_dict(torch.load(os.path.join(this_output_dir, 'model_f' + str(k) + '.ckpt')))
         # load_model.model.to(device)
         # load_model.model.eval()
+        #
         # compare_models(best_model, load_model.model)
-
-        # ###########################################################
-        #         child_counter = 0
-        #         for child in list(model_wrapper.model.children())[:-1]:
-        #             print(" child", child_counter, "is:")
-        #             print(child)
-        #             child_counter += 1
-        # ###########################################################
-
-        ## PYTORCH LIGHTNING IMPLEMENTATION - currently not working
-        #
-        # logger = TensorBoardLogger(save_dir=this_output_dir, version=k, name="lightning_logs")
-        # callbacks = [PrintCallback(),
-        #              EarlyStopping(monitor="val_loss",
-        #                            min_delta=.0,
-        #                            check_on_train_epoch_end=True,
-        #                            patience=10,
-        #                            check_finite=True,
-        #                            # stopping_threshold=1e-4,
-        #                            mode='min'),
-        #              ModelCheckpoint(dirpath=this_output_dir,
-        #                              filename='model_'+str(k)+'_{epoch}.pt',
-        #                              monitor='val_loss')
-        #              ]
-        #
-        # trainer = Trainer(
-        #     max_epochs=50,  # general
-        #     num_sanity_val_steps=0,
-        #     devices=1,
-        #     accelerator="auto",
-        #     # accelerator="GPU",
-        #     # auto_lr_find=True,
-        #     callbacks=callbacks,
-        #     default_root_dir=this_output_dir,
-        #     weights_save_path=this_output_dir,
-        #     logger=logger,
-        #
-        #     # fast_dev_run=True,  # debugging
-        #     # limit_train_batches=2,
-        #     # limit_val_batches=2,
-        #     # limit_test_batches=2,
-        #     # overfit_batches=1,
-        #
-        #     num_processes=1,  # HPC
-        #     # prepare_data_per_node=False,
-        #     # strategy="ddp",
-        # )
-        #
-        # trainer.fit(lightningmodule, train_dataloaders=datamodule.train_dataloader(), val_dataloaders=datamodule.val_dataloader())
-        # # trainer automatically saves statedict of last epoch, with early stopping this is the "best model"
-        # # trainer.save_checkpoint(os.path.join(this_output_dir, f"model.{k}.pt")) # -->> there is an automatic save, so not needed
-        #
-        #